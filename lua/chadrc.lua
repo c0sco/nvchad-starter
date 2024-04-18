80cbfc1780ae2f694f997ed5ab8f591e1c9326af
Matt Stofko <matt@mjslabs.com>
2024-04-17 19:32:53 -0700

    Using pwsh 7 for Windows

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -13,4 +13,15 @@ M.ui = {
 	-- },
 }
 
+-- Set our Windows shell to pwsh 7
+-- https://www.reddit.com/r/neovim/comments/pc7in0/detect_os_in_lua/
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.o.shell = "pwsh.exe"
+  vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';"
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+end
+
 return M
