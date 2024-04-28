615acf3dbd1a74f4d66e43daf10aa8011a8cef6f
Matt Stofko <matt@mjslabs.com>
2024-04-28 15:41:23 -0700

    Adding comment

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- Escape in terminal mode should leave insert mode, not close the buffer (which happens if this isn't here)
 map("t", "<Esc>", "<C-\\><C-n>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
